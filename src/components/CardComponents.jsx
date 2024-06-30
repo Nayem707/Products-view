2d46ace074f9333c8cc37899bdfe77b3107ac7e7
NAYEM ISLAM <gangsn490@gmail.com>
2024-06-29 21:08:27 -0600

    first commite

---
+++ src/components/CardComponents.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const CardComponents = () => {
+  return <div>CardComponents</div>;
+};
---
+++ src/components/PaginationComponents.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PaginationComponents = () => {
+  return <div>PaginationComponents</div>;
+};
+
+export default PaginationComponents;
