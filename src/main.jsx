761077702cb5dc8488217398ff20bff78d8fea5f
NAYEM ISLAM <gangsn490@gmail.com>
2024-06-29 22:26:38 -0600

    feat: add card componets and update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { RouterProvider } from 'react-router-dom';
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
+import { productsApi } from './features/productApiSlice';
+import { router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApiProvider api={productsApi}>
+      <RouterProvider router={router} />
+    </ApiProvider>
+  </React.StrictMode>
+);
--- src/page/HomeView.jsx
+++ src/page/HomeView.jsx
@@ -1,13 +1,13 @@
 import { Layout } from 'antd';
 //import components folder
-import CardComponent from '../components/CardComponents';
+import CardComponents from '../components/CardComponents';
 //define ant-design layout
 const { Content } = Layout;
 
 const HomeView = () => {
   return (
     <Content style={{ padding: '20px' }}>
-      <CardComponent />
+      <CardComponents />
     </Content>
   );
 };
